aa1b0c6997a15c8c089b77e1cb6d4c58e22d370b
Kenneth Day <kennethday2019@u.northwestern.edu>
2021-07-04 08:44:54 -0700

    Fn can determine # of goal of last regulation goal for non-pso match

--- match_reader.R
+++ match_reader.R
@@ -13,9 +13,42 @@ match_reader <- function(match, team2, score){
   
   ## Determine who scored last in regulation
   pso_check <- str_locate_all(match, pattern = "\n\nPenalty shootout\n\n")
+  ## Case #1: Match does go to a penalty shoot-out (PSO)
   if (length(pso_check[[1]]) != 0){
     goal_list_start_n <- 1
     goal_list_end_n <- pso_check[[1]][1,1]
+    goal_list <- substr(match, goal_list_start_n, goal_list_end_n)
+    
+    goal_count <- str_locate_all(goal_list, pattern = " [0-9]{1,3}\\. / ")
+    goal_minute_ends <- str_locate_all(goal_list, pattern = "[0-9]\\. / ")
+    goal_count_n <- nrow(goal_count[[1]])
+    if (goal_count_n == 0){
+      last_goal_team <- 0
+    } else {
+      last_goal_number <- 0
+      for (i in 1:goal_count_n){
+        goal_i_min_start_n <- goal_count[[1]][i,1] + 1
+        goal_i_min_end_n <- goal_minute_ends[[1]][i,1]
+        goal_i_min <- as.integer(substr(goal_list, goal_i_min_start_n, goal_i_min_end_n))
+        if (goal_i_min <= 90){
+          last_goal_number <- i
+        }
+      }
+    }
+  } 
+  ## Case #2: Match does not go to a penalty shoot-out (PSO)
+  else {
+    ## Calculate number of goals scored in match
+    score_sep = str_locate_all(score, pattern = ":")
+    score_sep_n <- score_sep[[1]][1,1]
+    team1_goals <- as.integer(substr(score, 1, (score_sep_n - 1)))
+    team2_goals <- as.integer(substr(score, (score_sep_n + 1), str_length(score)))
+    goal_count_n <- team1_goals + team2_goals
+    
+    ## Isolate text with goals
+    goal_list_start_n <- 1
+    goal_list_end <- str_locate_all(match, pattern = "\n\n")
+    goal_list_end_n <- goal_list_end[[1]][goal_count_n, 1]
     goal_list <- substr(match, goal_list_start_n, goal_list_end_n)
     
     goal_count <- str_locate_all(goal_list, pattern = " [0-9]{1,3}\\. / ")
