3546d0563e28a7e9deb4a21382cb2a9021831d6a
Kenneth Day <kennethday2019@u.northwestern.edu>
2021-07-09 12:33:13 -0700

    Fn adds columns for previous match stats for subs and sub mins

--- output_table.R
+++ output_table.R
@@ -13,7 +13,7 @@ output_table <- function(matches, rankings, country_ids, comp_rankings){
   ot$t2_subs <- 0
   ot$t2_sub_mins <- 0
   
-  for (i in 1:5){
+  for (i in 1:match_count){
     match_link <- ot[i,6]
     match_input <- gettxt(match_link)
     match_output <- match_reader(match_input, ot[i,3], ot[i,4])
@@ -55,35 +55,109 @@ output_table <- function(matches, rankings, country_ids, comp_rankings){
   rownames(ot) <- NULL
   
   ## Insert columns that show stats for team1's previous matches
+  
+  ## Add columns to be filled in
   ot$Team1_last_date <- NA
   ot$Team1_previous_matches <- 0
+  ot$Team1_subs_per_game <- 0
+  ot$Team1_sub_min_per_game <- 0
+  ot$Team1_subs_previous <- 0
+  ot$Team1_sub_mins_previous <- 0
+  ot$Team1_subs_previous_3 <- 0
+  ot$Team1_sub_mins_previous_3 <- 0
+  
   for (i in 2:match_count){
-    matches_i_t1 <- filter(ot[1:(i-1),], Team1_ID == ot[i,3] & Comp == ot[i,7])[,c(4,8)]
-    matches_i_t2 <- filter(ot[1:(i-1),], Team2_ID == ot[i,3] & Comp == ot[i,7])[,c(4,8)]
+    
+    ## Create table of previous matches and sort by date
+    columns_use <- c(4,8,16,17)
+    matches_i_t1 <- filter(ot[1:(i-1),], Team1_ID == ot[i,3] & Comp == ot[i,7])[,columns_use]
+    matches_i_t2 <- filter(ot[1:(i-1),], Team2_ID == ot[i,3] & Comp == ot[i,7])[,columns_use]
     matches_i <- rbind(matches_i_t1, matches_i_t2)
-    matches_dates_i <- matches_i[,1]
+    matches_i <- matches_i[order(matches_i$Date),]
+    
+    ## Count number of matches played
     previous_matches_i <- nrow(matches_i)
     ot[i,25] <- previous_matches_i
+    
+    ## Compute subs and sub mins per game
+    subs_total <- sum(matches_i$t1_subs)
+    sub_mins_total <- sum(matches_i$t1_sub_mins)
+    if (previous_matches_i != 0){
+      ot[i,26] <- subs_total / previous_matches_i
+      ot[i,27] <- sub_mins_total / previous_matches_i
+    }
+    
+    ## Compute subs and sub mins in previous match
+    if (previous_matches_i != 0){
+      ot[i,28] <- matches_i[previous_matches_i,3]
+      ot[i,29] <- matches_i[previous_matches_i,4]
+    }
+    
+    ## Compute subs and sub mins in previous 3 matches
+    if (previous_matches_i >= 3){
+      ot[i,30] <- sum(matches_i[((previous_matches_i-2):previous_matches_i),3])
+      ot[i,31] <- sum(matches_i[((previous_matches_i-2):previous_matches_i),4])
+    }
+    
+    ## Determine last match played
+    matches_dates_i <- matches_i[,1]
     ## Case 1: not team's first match of the competition
     if (previous_matches_i != 0){
-      ot[i,24] <- sort(matches_dates_i)[previous_matches_i]
+      ot[i,24] <- matches_dates_i[previous_matches_i]
     }
   }
   ot$Team1_last_date <- as_date(ot$Team1_last_date)
   
-  ## Insert columns that shows stats for team2's previous matches
+  ## Insert columns that show stats for team2's previous matches
+  
+  ## Add columns to be filled in
   ot$Team2_last_date <- NA
   ot$Team2_previous_matches <- 0
+  ot$Team2_subs_per_game <- 0
+  ot$Team2_sub_min_per_game <- 0
+  ot$Team2_subs_previous <- 0
+  ot$Team2_sub_mins_previous <- 0
+  ot$Team2_subs_previous_3 <- 0
+  ot$Team2_sub_mins_previous_3 <- 0
+  
   for (i in 2:match_count){
-    matches_i_t1 <- filter(ot[1:(i-1),], Team1_ID == ot[i,1] & Comp == ot[i,7])[,c(4,8)]
-    matches_i_t2 <- filter(ot[1:(i-1),], Team2_ID == ot[i,1] & Comp == ot[i,7])[,c(4,8)]
+    
+    ## Create table of previous matches and sort by date
+    columns_use <- c(4,8,18,19)
+    matches_i_t1 <- filter(ot[1:(i-1),], Team1_ID == ot[i,1] & Comp == ot[i,7])[,columns_use]
+    matches_i_t2 <- filter(ot[1:(i-1),], Team2_ID == ot[i,1] & Comp == ot[i,7])[,columns_use]
     matches_i <- rbind(matches_i_t1, matches_i_t2)
-    matches_dates_i <- matches_i[,1]
+    matches_i <- matches_i[order(matches_i$Date),]
+    
+    ## Count number of matches played
     previous_matches_i <- nrow(matches_i)
-    ot[i,27] <- previous_matches_i
+    ot[i,33] <- previous_matches_i
+    
+    ## Compute subs and sub mins per game
+    subs_total <- sum(matches_i$t2_subs)
+    sub_mins_total <- sum(matches_i$t2_sub_mins)
+    if (previous_matches_i != 0){
+      ot[i,34] <- subs_total / previous_matches_i
+      ot[i,35] <- sub_mins_total / previous_matches_i
+    }
+    
+    ## Compute subs and sub mins in previous match
+    if (previous_matches_i != 0){
+      ot[i,36] <- matches_i[previous_matches_i,3]
+      ot[i,37] <- matches_i[previous_matches_i,4]
+    }
+    
+    ## Compute subs and sub mins in previous 3 matches
+    if (previous_matches_i >= 3){
+      ot[i,38] <- sum(matches_i[((previous_matches_i-2):previous_matches_i),3])
+      ot[i,39] <- sum(matches_i[((previous_matches_i-2):previous_matches_i),4])
+    }
+    
+    ## Determine last match played
+    matches_dates_i <- matches_i[,1]
     ## Case 1: not team's first match of the competition
     if (previous_matches_i != 0){
-      ot[i,26] <- sort(matches_dates_i)[previous_matches_i]
+      ot[i,32] <- matches_dates_i[previous_matches_i]
     }
   }
   ot$Team2_last_date <- as_date(ot$Team2_last_date)
